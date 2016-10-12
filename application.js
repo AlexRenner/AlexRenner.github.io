0770524816ffe983d9e2f0b956999b08a571139b
Alex Renner <arenner38@yahoo.com>
2016-10-11 22:24:50 -0500

    Removed console logs from js file

--- application.js
+++ application.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	// Event listener for navigating through tabs
 	$(".tab").on("click", function(event){
-		console.log($(this)[0].innerText);
 		// Change active tab
 		$(".tab").removeClass("active");
 		$(this).addClass("active");
