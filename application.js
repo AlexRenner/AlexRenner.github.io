ba48161f15b1f323579bd2d0c064cd769eab3a3e
Alex Renner <arenner38@yahoo.com>
2016-10-11 22:03:57 -0500

    Added jquery for displaying and hiding content

--- application.js
+++ application.js
@@ -6,7 +6,8 @@ $(document).ready(function(){
 		$(".tab").removeClass("active");
 		$(this).addClass("active");
 		// Remove displayed content
-
+		$(".content").addClass("hidden");
 		// Display new content
+		$("body").find("#" + $(this)[0].innerText).removeClass("hidden");
 	})
 });
