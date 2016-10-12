4598933f9fd46f850339864249add65265651aaa
Alex Renner <arenner38@yahoo.com>
2016-10-11 21:54:09 -0500

    Created and styled experience content

--- application.js
+++ application.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 	// Event listener for navigating through tabs
 	$(".tab").on("click", function(event){
-		console.log("success");
+		console.log($(this)[0].innerText);
+		// Change active tab
+		
+		// Remove displayed content
+
+		// Display new content
 	})
 });
